d99eb0817ca0a157bf70bba1ced9b1da089a4843
Lokeshbabu19 <47393940+Lokeshbabu19@users.noreply.github.com>
2019-02-07 00:06:35 +0530

    Add files via upload

---
+++ fourth.py
@@ -0,0 +1,7 @@
+list = [1,2,0,0,4,1,6,2,1,3]
+sum1=sum(list)
+x = list[0]+list[9]
+x1=list[1]+list[2]+list[3]+list[4]+list[5]+list[6]+list[7]+list[8]
+print ("Total:",sum1)
+print("Batsmen1 Score:",list[0],"+",list[9],"=",x)
+print("Batsmen2 Score:",list[1],"+",list[2],"+",list[3],"+",list[4],"+",list[5],"+",list[6],"+",list[7],"+",list[8],"=",x1)
---
+++ third.py
@@ -0,0 +1,8 @@
+Total = {"Rick": 85, "Amit": 42, "George": 53, "Tanya": 60, "Linda": 35}
+x = Total.get("Rick")
+x1=Total.get("Amit")
+x2=Total.get("George")
+x3=Total.get("Tanya")
+x4=Total.get("Linda")
+t=x+x1+x2+x3+x4
+print(t)
